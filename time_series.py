c2f612a3426e74af4ac8ce79bac0727add7f79a7
KatieGott <Katie.Gott@target.com>
2022-12-02 10:16:01 -0600

    completed course

--- time_series.py
+++ time_series.py
@@ -730,4 +730,131 @@ simulated_data = AR_object.generate_sample(nsample=5000)
 plot_acf(simulated_data, lags=30)
 plt.show()
 
+# Cointegration Modelss
+# even if the prices of two different assets both follow random walks, it is still possible
+#   that a linear combination of them is not a random walk
+# THis means that the linear combination could be forecastable (even if the two separate aren't)
+#   this is called cointegrated
+# Walking the dog on a leash analogy
+# Two steps to test for cointegration:
+#   Regress one against the toher to get slope
+#   Then, run the Augmented Dickey-Fuller test (Pt - cQt) to test for random walk
+# statsmodels also has a coint function that combines these steps
+from statsmodels.tsa.stattools import coint
+coint(P,Q) 
+
+# Exercises
+
+# Plot the prices separately
+plt.subplot(2,1,1)
+plt.plot(7.25*HO, label='Heating Oil')
+plt.plot(NG, label='Natural Gas')
+plt.legend(loc='best', fontsize='small')
+
+# Plot the spread
+plt.subplot(2,1,2)
+plt.plot(7.25*HO-NG, label='Spread')
+plt.legend(loc='best', fontsize='small')
+plt.axhline(y=0, linestyle='--', color='k')
+plt.show()
+
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Compute the ADF for HO and NG
+result_HO = adfuller(HO['Close'])
+print("The p-value for the ADF test on HO is ", result_HO[1])
+result_NG = adfuller(NG['Close'])
+print("The p-value for the ADF test on NG is ", result_NG[1])
+
+# Compute the ADF of the spread
+result_spread = adfuller(7.25 * HO - NG)
+print("The p-value for the ADF test on the spread is ", result_spread[1])
+
+# Import the statsmodels module for regression and the adfuller function
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+
+# Regress BTC on ETH
+ETH = sm.add_constant(ETH)
+result = sm.OLS(BTC,ETH).fit()
+
+# Compute ADF
+b = result.params[1]
+adf_stats = adfuller(BTC['Price'] - b*ETH['Price'])
+print("The p-value for the ADF test is ", adf_stats[1])
+
+# Case Study
+# Test for Random Walk
+# Take first differences
+# compute ACF and PACF
+# Fit a few AR, MA and ARMA models
+# Use information criterion to choose best model
+# forecase temp over next 30 years
+
+# Exercises:
+
+# Import the adfuller function from the statsmodels module
+from statsmodels.tsa.stattools import adfuller
+
+# Convert the index to a datetime object
+temp_NY.index = pd.to_datetime(temp_NY.index, format='%Y')
+
+# Plot average temperatures
+temp_NY.plot()
+plt.show()
+
+# Compute and print ADF p-value
+result = adfuller(temp_NY['TAVG'])
+print("The p-value for the ADF test is ", result[1])
+
+# Import the modules for plotting the sample ACF and PACF
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# Take first difference of the temperature Series
+chg_temp = temp_NY.diff()
+chg_temp = chg_temp.dropna()
+
+# Plot the ACF and PACF on the same page
+fig, axes = plt.subplots(2,1)
+
+# Plot the ACF
+plot_acf(chg_temp, lags=20, ax=axes[0])
+
+# Plot the PACF
+plot_pacf(chg_temp, lags=20, ax=axes[1])
+plt.show()
+
+# Import the module for estimating an ARIMA model
+from statsmodels.tsa.arima.model import ARIMA
+
+# Fit the data to an AR(1) model and print AIC:
+mod_ar1 = ARIMA(chg_temp, order=(1,0,0))
+res_ar1 = mod_ar1.fit()
+print("The AIC for an AR(1) is: ", res_ar1.aic)
+
+# Fit the data to an AR(2) model and print AIC:
+mod_ar2 = ARIMA(chg_temp, order=(2,0,0))
+res_ar2 = mod_ar2.fit()
+print("The AIC for an AR(2) is: ", res_ar2.aic)
+
+# Fit the data to an ARMA(1,1) model and print AIC:
+mod_arma11 = ARIMA(chg_temp, order=(1,0,1))
+res_arma11 = mod_arma11.fit()
+print("The AIC for an ARMA(1,1) is: ", res_arma11.aic)
+
+# Import the ARIMA module from statsmodels
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_predict
+
+# Forecast temperatures using an ARIMA(1,1,1) model
+mod = ARIMA(temp_NY, trend='t', order=(1,1,1))
+res = mod.fit()
+
+# Plot the original series and the forecasted series
+fig, ax = plt.subplots()
+temp_NY.plot(ax=ax)
+plot_predict(res, start='1872', end='2046', ax=ax)
+plt.show()
+
 
